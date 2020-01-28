3910f5bf717545ff4d4c29327cbd1b28b51ca516
Danny Seymour <danny.saseymour@gmail.com>
2020-01-28 05:20:31 -0500

    update before refactoring project structure

--- Chillflixapi/Models/Repositories/IUserRepository.cs
+++ Chillflixapi/Models/Repositories/IUserRepository.cs
@@ -4,5 +4,6 @@ namespace Chillflixapi.Models.Repositories
     public interface IUserRepository
     {
         //https://www.notion.so/dannysas/IUserRepository-5cd139967c4e47f59abe460487760422
+
     }
 }
