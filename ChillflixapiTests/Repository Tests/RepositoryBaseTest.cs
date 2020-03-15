c443c05ce770c9da20c8274374a7c7a326b54613
Danny Seymour <danny.saseymour@gmail.com>
2020-03-14 22:19:48 -0400

    starting to implement tests

---
+++ ChillflixapiTests/Repository Tests/RepositoryBaseTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xunit;
+
+
+namespace ChillflixapiTests.RepositoryTests
+{
+    public class RepositoryBaseTest
+    {
+        public RepositoryBaseTest()
+        {
+
+
+        }
+
+
+
+        [Fact]
+        public IQueryable<TEntity> GetAll(bool trackChanges)
+        {
+            
+
+  //      public IQueryable<TEntity> GetWhere(Expression<Func<TEntity, bool>> expression, bool trackChanges);
+    //    public Task<TEntity> GetById(Tkey id, bool trackChanges);
+      //  publicTask<TEntity> FirstOrDefault(Expression<Func<TEntity, bool>> predicate, bool trackChanges);
+
+        //public void Add(TEntity entity);
+        //public void Update(TEntity entity);
+        //public void Delete(TEntity entity);
+
+      //  public Task<int> CountAll();
+       // public Task<int> CountWhere(Expression<Func<TEntity, bool>> predicate);
+
+
+
+
+    }
+}
