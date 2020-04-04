f3f2e1431a703a4c5f6b395e139d68422a532660
Danny Seymour <danny.saseymour@gmail.com>
2020-04-04 13:42:30 -0400

    removed microsoft.extensions.logging from services project was creating version conflicts with main project

---
+++ Chillflixapi/ActionFilters/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chillflixapi.ActionFilters
+{
+    public class Class1
+    {
+    }
+}
